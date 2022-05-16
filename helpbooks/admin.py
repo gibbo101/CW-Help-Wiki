0acf148ea623e921214198509f399ba8b1580d8d
Luke Gibson <lukegibson101@gmail.com>
2022-05-16 22:25:41 +0000

    add Categorys

--- helpbooks/admin.py
+++ helpbooks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 from .models import Category, Subject, Comments
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug')
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
